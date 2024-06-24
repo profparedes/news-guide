a23248a63b7e8fc766f2c325982fc209c2b47de0
Fábio Paredes <f.paredes.o@gmail.com>
2024-06-24 19:42:52 -0300

    installing dependencies

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,27 +1,18 @@
 import { memo, useEffect } from 'react'
 
-import { useConfig } from 'config/config'
-import { useTranslation } from 'react-i18next'
-
-import LanguageSwitcher from 'components/LanguageSwitcher'
-
 import useTitle from 'hooks/useTitle'
 
 const Home: React.FC = () => {
-  const { app } = useConfig()
-  const { t, i18n } = useTranslation()
   const setTitle = useTitle()
 
   useEffect(() => {
-    setTitle(t('home.head-title'))
+    setTitle('Home')
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [i18n.resolvedLanguage])
+  }, [])
 
   return (
     <>
-      <h1>{t('home.title')}</h1>
-      <LanguageSwitcher />
-      <p>{`v${app.version}`}</p>
+      <h1>NEWS GUIDE</h1>
     </>
   )
 }
